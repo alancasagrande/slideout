8df006491fdf61fa15a6ffb00474550803173268
Sebastian Welther <swelther@siewert-kau.de>
2015-08-19 17:02:20 +0200

    make diff calculation work with right side slideout menu

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ Slideout.prototype._initTouchEvents = function() {
 
     self._moved = false;
     self._opening = false;
-    var offset = eve.touches[0].pageX;
+    var offset = self._orientation === 1 ? eve.touches[0].pageX : window.innerWidth - eve.touches[0].pageX;
     self._startOffsetX = offset;
     self._preventOpen = (!self._touch || (!self.isOpen() && (self.menu.clientWidth !== 0 || (self._grabWidth && offset > self._grabWidth))));
   };
